3b5a80d088d1a5a2ed4a05abfc42d7be0a8568fe
Aman Tailor <amantailorpw@Aman-12605.local>
2025-08-05 22:03:50 +0530

    feat: initialize project

---
+++ App.js
@@ -0,0 +1,18 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+import IngredientScreen from './src/screens/IngredientScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Ingredients" component={IngredientScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
